47bf05e01648a909fd0d74afda0091ca91c3c3c7
Rohan Agrawal <send2arohan@gmail.com>
2016-12-29 11:15:22 -0800

    print errors and warnings to stderr

--- ros2log/include/ros2log/scoped_sinks.hpp
+++ ros2log/include/ros2log/scoped_sinks.hpp
@@ -72,10 +72,10 @@ class ScopedPrintSink {
           switch (message.level) {
             case Log_Levels::FATAL:
             case Log_Levels::ERROR:
-              printf("\x1b[31m%s\x1b[0m\n", log_string.c_str());
+              fprintf(stderr, "\x1b[31m%s\x1b[0m\n", log_string.c_str());
               break;
             case Log_Levels::WARN:
-              printf("\x1b[33m%s\x1b[0m\n", log_string.c_str());
+              fprintf(stderr, "\x1b[33m%s\x1b[0m\n", log_string.c_str());
               break;
             case Log_Levels::DEBUG:
               printf("\x1b[32m%s\x1b[0m\n", log_string.c_str());
